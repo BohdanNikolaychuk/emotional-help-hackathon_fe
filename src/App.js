a8a3d8874fa0587595bbdbf09e7cc65da3f698eb
Bohdan <nikolaychukbm@gmail.com>
2022-11-23 11:38:47 +0200

    add basic routing/create home and selftest pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SelfTest from './pages/SelfTest/SelfTest';
 
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/selftest">About</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/selftest" element={<SelfTest />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/SelfTest/SelfTest.jsx
@@ -0,0 +1,5 @@
+function SelfTest() {
+  return <h2>SelfTest</h2>;
+}
+
+export default SelfTest;
