b5ec9d665ae8f55f9055dcf3d3a036ee5cefb472
Katarhu <mkaimg1@gmail.com>
2022-12-11 16:42:10 +0200

    New Header

--- src/components/OldHeader/components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import logoImage from '../../../../assets/Logo.svg';
-
-function Logo() {
-    return (
-        <img style={{objectFit: "cover", maxWidth: "100%", maxHeight: "100%"}} src={logoImage} alt="Medical logo"/>
-    );
-}
-
-export default Logo;
